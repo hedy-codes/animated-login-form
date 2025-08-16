8413efb8eee8830b50e36c0c21761ae3f9d959b3
Web Developer <hedyeazizi0752@gmail.com>
2025-08-16 23:36:33 +0330

    add : Variable Definition in JavaScript

---
+++ js/script.js
@@ -0,0 +1,3 @@
+const signinbtnLink = document.querySelector('.signinbtn-link');
+const signupbtnLink = document.querySelector('.signupbtn-link');
+const wrapper = document.querySelector('.wrapper');
